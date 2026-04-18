7b8b0139c7fa51fdaf5fbe659d4beefc61dc81d0
alistair <2164516644@qq.com>
2026-04-18 15:54:56 +0800

    update powershell config

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,7 @@ carapace _carapace | Out-String | Invoke-Expression
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 # 提示建议以列表方式展现
 Set-PSReadLineOption -PredictionViewStyle ListView
-
+# 初始化 starship （美化提示符）
 Invoke-Expression (&starship init powershell)
-
+# 初始化 zoxide （z 智能调整）
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
